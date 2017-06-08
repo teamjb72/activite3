0b970770f91d033da00d5b056f34e43fec100ba0
Windows <team_jb@hotmail.com>
2017-06-08 14:09:10 +0200

    test

--- src/OC/PlatformBundle/DataFixtures/ORM/LoadAdvert.php
+++ src/OC/PlatformBundle/DataFixtures/ORM/LoadAdvert.php
@@ -5,7 +5,7 @@ namespace OC\PlatformBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-use OC\PlatformBundle\Entity\Skill;
+use OC\PlatformBundle\Entity\Advert;
 
 class LoadAdvert implements FixtureInterface
 {
