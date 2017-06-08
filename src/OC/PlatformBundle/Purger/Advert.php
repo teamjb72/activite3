45b0eb2601bef3eb96a3a36c752c1276961fd0de
Windows <team_jb@hotmail.com>
2017-06-08 16:55:56 +0200

    test

--- src/OC/PlatformBundle/Purger/Advert.php
+++ src/OC/PlatformBundle/Purger/Advert.php
@@ -32,22 +32,21 @@ class Advert
         $advertRepository      = $this->em->getRepository('OCPlatformBundle:Advert');
         $advertSkillRepository = $this->em->getRepository('OCPlatformBundle:AdvertSkill');
 
-        // On récupère les annonces à supprimer
         $listAdverts = $advertRepository->getOldAdverts($days);
-        // On parcourt les annonces pour les supprimer effectivement
+
         foreach ($listAdverts as $advert) {
 
             $advertSkills = $advertSkillRepository->findBy(array('advert' => $advert));
-            // Pour les supprimer toutes avant de pouvoir supprimer l'annonce elle-même
-            /*foreach ($advertSkills as $advertSkill) {
+
+            foreach ($advertSkills as $advertSkill) {
                 $this->em->remove($advertSkill);
-            }*/
-           /* // On peut maintenant supprimer l'annonce
-            $this->em->remove($advert);*/
+            }
+
+            $this->em->remove($advert);
            echo $advert->getTitle()."<br>;";
         }
-        // Et on n'oublie pas de faire un flush !
-      /*  $this->em->flush();*/
+
+       $this->em->flush();
 
     }
 }
