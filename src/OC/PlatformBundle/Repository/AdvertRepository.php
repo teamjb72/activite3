b0cae562a43012ffb06a20eca72b535824d407eb
Windows <team_jb@hotmail.com>
2017-06-08 15:25:31 +0200

    test

--- src/OC/PlatformBundle/Repository/AdvertRepository.php
+++ src/OC/PlatformBundle/Repository/AdvertRepository.php
@@ -32,7 +32,7 @@ class AdvertRepository extends EntityRepository
     return new Paginator($query, true);
   }
 
-  public function getOldAdverts($days)
+  public function getAdvertsBefore($days)
   {
 
     return $this->createQueryBuilder('a')
