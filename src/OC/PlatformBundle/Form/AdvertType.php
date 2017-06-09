1d9ee56cd909628396d62eb6588547fc6be9a50f
Ubuntu <ubuntu@ip-172-31-15-205.eu-west-2.compute.internal>
2017-06-09 15:26:05 +0000

    lll

---
+++ src/OC/PlatformBundle/Form/AdvertType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace OC\PlatformBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdvertType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('date')->add('title')->add('author')->add('content')->add('published')->add('updatedAt')->add('nbApplications')->add('slug')->add('image')->add('categories');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'OC\PlatformBundle\Entity\Advert'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'oc_platformbundle_advert';
+    }
+
+
+}
