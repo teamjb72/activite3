85a333a6bef5ff4cc83806fae991bb148f8a0905
Windows <team_jb@hotmail.com>
2017-06-09 14:38:12 +0200

    test

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -6,9 +6,12 @@ namespace OC\PlatformBundle\Controller;
 
 use OC\PlatformBundle\Entity\Advert;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class AdvertController extends Controller
 {
@@ -79,17 +82,31 @@ class AdvertController extends Controller
 
   public function addAction(Request $request)
   {
-    $em = $this->getDoctrine()->getManager();
+      // On crée un objet Advert
+      $advert = new Advert();
 
-    // On ne sait toujours pas gérer le formulaire, patience cela vient dans la prochaine partie !
+      // On crée le FormBuilder grâce au service form factory
+      $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $advert);
 
-    if ($request->isMethod('POST')) {
-      $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+      // On ajoute les champs de l'entité que l'on veut à notre formulaire
+      $formBuilder
+          ->add('date',      DateType::class)
+          ->add('title',     TextType::class)
+          ->add('content',   TextareaType::class)
+          ->add('author',    TextType::class)
+          ->add('published', CheckboxType::class)
+          ->add('save',      SubmitType::class)
+      ;
+      // Pour l'instant, pas de candidatures, catégories, etc., on les gérera plus tard
 
-      return $this->redirectToRoute('oc_platform_view', array('id' => $advert->getId()));
-    }
+      // À partir du formBuilder, on génère le formulaire
+      $form = $formBuilder->getForm();
 
-    return $this->render('OCPlatformBundle:Advert:add.html.twig');
+      // On passe la méthode createView() du formulaire à la vue
+      // afin qu'elle puisse afficher le formulaire toute seule
+      return $this->render('OCPlatformBundle:Advert:add.html.twig', array(
+          'form' => $form->createView(),
+      ));
   }
 
   public function editAction($id, Request $request)
