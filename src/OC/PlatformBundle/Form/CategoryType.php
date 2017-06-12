bc90089418b34d3cee7b72acd379794796e7e190
Windows <team_jb@hotmail.com>
2017-06-12 15:15:39 +0200

    test
    # Conflicts:
    #	src/OC/PlatformBundle/Resources/config/services.yml

--- src/OC/PlatformBundle/Form/CategoryType.php
+++ src/OC/PlatformBundle/Form/CategoryType.php
@@ -1,38 +1,25 @@
 <?php
+// src/OC/PlatformBundle/Form/CategoryType.php
 
 namespace OC\PlatformBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CategoryType extends AbstractType
 {
-    /**
-     * {@inheritdoc}
-     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name');
+        $builder
+            ->add('name', TextType::class);
     }
-    
-    /**
-     * {@inheritdoc}
-     */
+
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
             'data_class' => 'OC\PlatformBundle\Entity\Category'
         ));
     }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getBlockPrefix()
-    {
-        return 'oc_platformbundle_category';
-    }
-
-
 }
