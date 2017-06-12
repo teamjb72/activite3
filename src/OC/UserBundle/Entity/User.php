1876583dfcfaabf4d0446605774f7b92c9f50503
Windows <team_jb@hotmail.com>
2017-06-12 18:03:47 +0200

    test
    # Conflicts:
    #	src/OC/PlatformBundle/Resources/config/services.yml

--- src/OC/UserBundle/Entity/User.php
+++ src/OC/UserBundle/Entity/User.php
@@ -2,144 +2,44 @@
 
 namespace OC\UserBundle\Entity;
 
-use Symfony\Component\Security\Core\User\UserInterface;
-
+use Doctrine\ORM\Mapping as ORM;
 
 /**
- * User
+ * @ORM\Table(name="oc_user")
+ * @ORM\Entity(repositoryClass="OC\UserBundle\Entity\UserRepository")
  */
 class User
 {
     /**
-     * @var int
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
 
     /**
-     * @var string
+     * @ORM\Column(name="username", type="string", length=255, unique=true)
      */
     private $username;
 
     /**
-     * @var string
+     * @ORM\Column(name="password", type="string", length=255)
      */
     private $password;
 
     /**
-     * @var array
+     * @ORM\Column(name="salt", type="string", length=255)
      */
     private $salt;
 
     /**
-     * @var array
+     * @ORM\Column(name="roles", type="array")
      */
-    private $roles;
+    private $roles = array();
 
+    // Les getters et setters
 
-    /**
-     * Get id
-     *
-     * @return int
-     */
-    public function getId()
+    public function eraseCredentials()
     {
-        return $this->id;
-    }
-
-    /**
-     * Set username
-     *
-     * @param string $username
-     *
-     * @return User
-     */
-    public function setUsername($username)
-    {
-        $this->username = $username;
-
-        return $this;
-    }
-
-    /**
-     * Get username
-     *
-     * @return string
-     */
-    public function getUsername()
-    {
-        return $this->username;
-    }
-
-    /**
-     * Set password
-     *
-     * @param string $password
-     *
-     * @return User
-     */
-    public function setPassword($password)
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    /**
-     * Get password
-     *
-     * @return string
-     */
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
-    /**
-     * Set salt
-     *
-     * @param array $salt
-     *
-     * @return User
-     */
-    public function setSalt($salt)
-    {
-        $this->salt = $salt;
-
-        return $this;
-    }
-
-    /**
-     * Get salt
-     *
-     * @return array
-     */
-    public function getSalt()
-    {
-        return $this->salt;
-    }
-
-    /**
-     * Set roles
-     *
-     * @param array $roles
-     *
-     * @return User
-     */
-    public function setRoles($roles)
-    {
-        $this->roles = $roles;
-
-        return $this;
-    }
-
-    /**
-     * Get roles
-     *
-     * @return array
-     */
-    public function getRoles()
-    {
-        return $this->roles;
     }
 }
-
